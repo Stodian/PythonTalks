52d5af049cee2f5dea213ed1106e112e63950f5c
Ethan Reid <163367994+Stodian@users.noreply.github.com>
2024-06-18 01:01:11 +0100

    sensor data

---
+++ sensordata.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+TRIG = 23
+ECHO = 24
+
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
+
+def get_distance():
+    GPIO.output(TRIG, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG, False)
+
+    start_time = time.time()
+    stop_time = time.time()
+
+    while GPIO.input(ECHO) == 0:
+        start_time = time.time()
+
+    while GPIO.input(ECHO) == 1:
+        stop_time = time.time()
+
+    time_elapsed = stop_time - start_time
+    distance = (time_elapsed * 34300) / 2
+    return distance
+
+try:
+    while True:
+        dist = get_distance()
+        print(f"Measured Distance = {dist:.1f} cm")
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Measurement stopped by User")
+    GPIO.cleanup()
